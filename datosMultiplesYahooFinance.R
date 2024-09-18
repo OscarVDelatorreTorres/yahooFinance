10b90cf70697182661036f46d533e513fd765294
Dr. Oscar V. De la Torre Torres <110574678+OscarVDelatorreTorres@users.noreply.github.com>
2024-09-18 09:01:58 -0600

    Update datosMultiplesYahooFinance.R

--- datosMultiplesYahooFinance.R
+++ datosMultiplesYahooFinance.R
@@ -1,4 +1,4 @@
-# Arcivo de respaldo para ejecutar los compandos para la extracción de datos 
+# Arcivo de respaldo para ejecutar los compandos para la extracción de datos
 # de Yahoo Finance:
 
 # Autor: Dr. Oscsr V. De la Torre-Torres
@@ -27,10 +27,10 @@ if (!require(imputeTS)) {install.packages('imputeTS')
   library(imputeTS)} else {library(imputeTS)}
 
 # Comandos a ejecutar para extraer información de Yahoo para múltiples RICS:====
- 
+
 
 historico_multiples_precios=function(tickers,de,hasta,periodicidad="D",fxRate="USDMXN=X",whichToFX="none"){
-  
+
   nombres=tickers
   convertFXOk=FALSE
   # Crea los nombres de la tabla de salida sin caracteres especiales de los tickers de ?ndices:
@@ -38,29 +38,29 @@ historico_multiples_precios=function(tickers,de,hasta,periodicidad="D",fxRate="U
     if (substr(nombres[cuenta],1,1)=="^"){
       nombres[cuenta]=substr(tickers[cuenta],2,nchar(tickers[cuenta]))
     }
-    
+
     nombres[cuenta]=str_replace(nombres[cuenta],"=","")
-    
+
   }
-  
+
   if (is.logical(whichToFX)) {
-    
+
     if (length(whichToFX)==length(tickers)){
       convertFXOk=TRUE
-      
+
     } else {
       stop("El vector de conversión a moneda  whichToFX no tiene la misma longitud que el vector de tickers.")
     }
-    
+
   } else {
 
     if (is.character(whichToFX)) {
       switch(whichToFX,
-             "All"={ whichToFX=rep(TRUE,length(tickers)) 
+             "All"={ whichToFX=rep(TRUE,length(tickers))
              convertFXOk=TRUE},
-             "all"={ whichToFX=rep(TRUE,length(tickers)) 
+             "all"={ whichToFX=rep(TRUE,length(tickers))
              convertFXOk=TRUE},
-             "ALL"={ whichToFX=rep(TRUE,length(tickers)) 
+             "ALL"={ whichToFX=rep(TRUE,length(tickers))
              convertFXOk=TRUE},
              "NONE"={ whichToFX=rep(FALSE,length(tickers)) },
              "none"={ whichToFX=rep(FALSE,length(tickers)) },
@@ -71,191 +71,191 @@ historico_multiples_precios=function(tickers,de,hasta,periodicidad="D",fxRate="U
 
 
   }
-  
-  
+
+
   #length(nombres)
   for (cuenta in 1:length(nombres)){
     # Extrae 1 a 1 los hist?ricos de cada ticker y forma la tabal de salida
-    
+
     if (isTRUE(whichToFX[cuenta])){
-      
+
       queryString=paste0("datosYahoo=historico_precio_mkts('",tickers[cuenta],
                          "',de=de,hasta=hasta,periodicidad=periodicidad,fxRate='",
                          fxRate,"')")
-      
+
     } else {
-      
+
       queryString=paste0("datosYahoo=historico_precio_mkts('",tickers[cuenta],
                          "',de=de,hasta=hasta,periodicidad=periodicidad,fxRate='none')")
-      
+
     }
-    
+
     cat("\f")
     print(paste0("Extrayendo RIC ",cuenta," de ",length(tickers)," (",
                  tickers[cuenta],"), periodicidad ",periodicidad))
-    
-    eval(parse(text=queryString))
-    
 
-    
+    eval(parse(text=queryString))
+
+
+
     # Anexa las columnas de datos a la tabla total de salida:
-    
+
     if (cuenta<2){
-      
+
       # Guarda lo extraído de precios al objeto tipo "lista" de salida:
-      
-      eval(parse(text=paste0("conjuntoSalida=list(",nombres[cuenta],"=as.data.frame(datosYahoo$",nombres[cuenta],"))")))       
-      
-    
+
+      eval(parse(text=paste0("conjuntoSalida=list(",nombres[cuenta],"=as.data.frame(datosYahoo$",nombres[cuenta],"))")))
+
+
       # Inserta valores de la primera serie de tiempo en la tabla de precios de salida:
-      
+
       eval(parse(text=paste0("tablaDatos=conjuntoSalida$",nombres[cuenta])))
-      
+
       tablaString=paste0("tabla.precios=data.frame(Date=tablaDatos$date,",
                          nombres[cuenta],
                          "=tablaDatos$adjusted)")
-      
+
       eval(parse(text=tablaString))
-      
+
       tablaString=paste0("tabla.precios$",nombres[cuenta],"=na_locf(tabla.precios$",nombres[cuenta],",option='locf')")
-      
+
       eval(parse(text=tablaString))
-      
+
       # Inserta valores de la primera serie de tiempo en la tabla de P/L de salida:
       tablaString2=paste0("tabla.PL=data.frame(Date=tablaDatos$date,",
                           nombres[cuenta],
                           "=tablaDatos$PL)")
-      
-      eval(parse(text=tablaString2))  
+
+      eval(parse(text=tablaString2))
       tabla.PL=tabla.PL[-1,]
-      
+
       tablaString2=paste0("tabla.PL$",nombres[cuenta],
                          "=na_replace(tabla.PL$",nombres[cuenta],",fill=0)")
-      
+
       eval(parse(text=tablaString2))
-      
-      
+
+
       # Inserta valores de la primera serie de tiempo en la tabla de rendimientos aritméticos de salida:
       tablaString3=paste0("tabla.rendimientosArit=data.frame(Date=tablaDatos$date,",
                           nombres[cuenta],
                           "=tablaDatos$rArit)")
-      
-      eval(parse(text=tablaString3))    
-      
+
+      eval(parse(text=tablaString3))
+
       tabla.rendimientosArit=tabla.rendimientosArit[-1,]
-      
+
       tablaString3=paste0("tabla.rendimientosArit$",nombres[cuenta],
                          "=na_replace(tabla.rendimientosArit$",nombres[cuenta],",fill=0)")
-      
+
       eval(parse(text=tablaString3))
-      
+
       # Inserta valores de la primera serie de tiempo en la tabla de rendimientos aritméticos de salida:
       tablaString4=paste0("tabla.rendimientosCont=data.frame(Date=tablaDatos$date,",
                           nombres[cuenta],
                           "=tablaDatos$rCont)")
-      
-      eval(parse(text=tablaString4))  
-      
+
+      eval(parse(text=tablaString4))
+
       tabla.rendimientosCont=tabla.rendimientosCont[-1,]
-      
+
       tablaString4=paste0("tabla.rendimientosCont$",nombres[cuenta],
                           "=na_replace(tabla.rendimientosCont$",nombres[cuenta],",fill=0)")
-      
-      eval(parse(text=tablaString4))          
-      
+
+      eval(parse(text=tablaString4))
+
   # Else de cuenta cuando else>=2:----------------------------------
     } else {
-      
+
       # Guarda lo extraído de precios al objeto tipo "lista" de salida:
-      
-      eval(parse(text=paste0("conjuntoSalida$",nombres[cuenta],"=datosYahoo$",nombres[cuenta])))       
-      
-      
-      #eval(parse(text=paste0("conjuntoSalida[['",nombres[cuenta],"']]=",nombres[cuenta])))       
-      
+
+      eval(parse(text=paste0("conjuntoSalida$",nombres[cuenta],"=datosYahoo$",nombres[cuenta])))
+
+
+      #eval(parse(text=paste0("conjuntoSalida[['",nombres[cuenta],"']]=",nombres[cuenta])))
+
       # Inserta valores de la primera serie de tiempo en la tabla de precios de salida:
-      
+
       eval(parse(text=paste0("tablaDatos=conjuntoSalida$",nombres[cuenta])))
-      
+
       tablaString=paste0("tabla.preciosb=data.frame(Date=tablaDatos$date,",
                          nombres[cuenta],
                          "=tablaDatos$adjusted)")
-      
+
       eval(parse(text=tablaString))
-      
+
       tablaString=paste0("tabla.preciosb$",nombres[cuenta],
                          "=na_locf(tabla.preciosb$",nombres[cuenta],",option='locf')")
-      
+
       eval(parse(text=tablaString))
-      
+
       tabla.precios=merge(tabla.precios,tabla.preciosb,by="Date",all=F)
-      
+
       # Inserta valores de la primera serie de tiempo en la tabla de P/L de salida:
       tablaString2=paste0("tabla.PLb=data.frame(Date=tablaDatos$date,",
                           nombres[cuenta],
                           "=tablaDatos$PL)")
-      
-      eval(parse(text=tablaString2))  
+
+      eval(parse(text=tablaString2))
       tabla.PLb=tabla.PLb[-1,]
-      
+
       tablaString2=paste0("tabla.PLb$",nombres[cuenta],
                           "=na_replace(tabla.PLb$",nombres[cuenta],",fill=0)")
-      
+
       eval(parse(text=tablaString2))
-      
+
       tabla.PL=merge(tabla.PL,tabla.PLb,by="Date",all=F)
-      
+
       # Inserta valores de la primera serie de tiempo en la tabla de rendimientos aritméticos de salida:
       tablaString3=paste0("tabla.rendimientosAritb=data.frame(Date=tablaDatos$date,",
                           nombres[cuenta],
                           "=tablaDatos$rArit)")
-      
-      eval(parse(text=tablaString3))    
-      
+
+      eval(parse(text=tablaString3))
+
       tabla.rendimientosAritb=tabla.rendimientosAritb[-1,]
-      
+
       tablaString3=paste0("tabla.rendimientosAritb$",nombres[cuenta],
                           "=na_replace(tabla.rendimientosAritb$",nombres[cuenta],",fill=0)")
-      
+
       eval(parse(text=tablaString3))
-      
+
       tabla.rendimientosArit=merge(tabla.rendimientosArit,tabla.rendimientosAritb,by="Date",all=F)
-      
+
       # Inserta valores de la primera serie de tiempo en la tabla de rendimientos aritméticos de salida:
       tablaString4=paste0("tabla.rendimientosContb=data.frame(Date=tablaDatos$date,",
                           nombres[cuenta],
                           "=tablaDatos$rCont)")
-      
-      eval(parse(text=tablaString4))  
-      
+
+      eval(parse(text=tablaString4))
+
       tabla.rendimientosContb=tabla.rendimientosContb[-1,]
-      
+
       tablaString4=paste0("tabla.rendimientosContb$",nombres[cuenta],
                           "=na_replace(tabla.rendimientosContb$",nombres[cuenta],",fill=0)")
-      
-      eval(parse(text=tablaString4))   
-      
+
+      eval(parse(text=tablaString4))
+
       tabla.rendimientosCont=merge(tabla.rendimientosCont,tabla.rendimientosContb,by="Date",all=F)
-      
-  # Else cuenta >=2 termina aquí:    
+
+  # Else cuenta >=2 termina aquí:
     }
-    
-    # cuenta loop ends here:  
-  }  
+
+    # cuenta loop ends here:
+  }
 
   colnames(tabla.precios)=c("Date",nombres)
   colnames(tabla.PL)=c("Date",nombres)
   colnames(tabla.rendimientosArit)=c("Date",nombres)
   colnames(tabla.rendimientosCont)=c("Date",nombres)
-  
+
   conjuntoSalida[["tablaPrecios"]]=tabla.precios
   conjuntoSalida[["tablaPL"]]=tabla.PL
   conjuntoSalida[["tablaRendimientosArit"]]=tabla.rendimientosArit
   conjuntoSalida[["tablaRendimientosCont"]]=tabla.rendimientosCont
-  
+
   cat("\f")
   print(paste0("Se terminó de extraer y procesar un total de ",length(tickers),
-               " tickers desde las BD de Yahoo Finance..."))  
+               " tickers desde las BD de Yahoo Finance..."))
   print(paste0("Tickers procesados: ",paste0(tickers,collapse=", ")))
   return(conjuntoSalida)
   # function ends here:
@@ -265,46 +265,46 @@ historico_multiples_precios=function(tickers,de,hasta,periodicidad="D",fxRate="U
 # Comando de extracción de precios de ticker individual ====
 historico_precio_mkts <- function(ticker,de,hasta,periodicidad,fxRate)
 {
- 
+
    stringTicker=substr(ticker,1,1)
-  
+
   if (stringTicker=="^"){
-    ticker2=paste0("%5E",substr(ticker,2,nchar(ticker))) 
+    ticker2=paste0("%5E",substr(ticker,2,nchar(ticker)))
     nombre=substr(ticker,2,nchar(ticker))
   } else {
     ticker2=ticker
     nombre=ticker
   }
-  
-  
-    
+
+
+
 # Extrae datos históricos de cotizaciones con tidyquant:
 
 print(paste0("Extrayendo ",ticker,"..."))
-   
+
 tablaDatos = tq_get(ticker, from = de, to  = hasta)
 
 # Convierte los datos originales al tipo de cambio deseado:
 
 if (!(fxRate=="none")){
-  
+
 print(paste0("Convirtiendo ",ticker," con paridad cambiaria ",fxRate,"..."))
-  
+
   tablaDatosFX = tq_get(fxRate, from = de, to  = hasta)
   tablaDatosFX=data.frame(date=tablaDatosFX$date,
                           FX=tablaDatosFX$adjusted)
   tablaDatosFX=tablaDatos%>%merge(tablaDatosFX,by="date",all.x=T)
-  
+
   tablaDatosFX=data.frame(date=tablaDatosFX$date,
-                          FX=tablaDatosFX$FX)  
+                          FX=tablaDatosFX$FX)
   tablaDatosFX$FX=na_locf(tablaDatosFX$FX,option="locf")
-  
+
   tablaDatos$open=tablaDatos$open*tablaDatosFX$FX
   tablaDatos$high=tablaDatos$high*tablaDatosFX$FX
   tablaDatos$low=tablaDatos$low*tablaDatosFX$FX
   tablaDatos$close=tablaDatos$close*tablaDatosFX$FX
   tablaDatos$adjusted=tablaDatos$adjusted*tablaDatosFX$FX
-  
+
   }
 
 
@@ -312,37 +312,37 @@ print(paste0("Convirtiendo ",ticker," con paridad cambiaria ",fxRate,"..."))
 
 switch(periodicidad,
        "W"={
-         print(paste0("Convirtiendo ",ticker," a frecuencia",periodicidad2,"..."))          
+         print(paste0("Convirtiendo ",ticker," a frecuencia",periodicidad,"..."))
          tablaDatos=tablaDatos%>%tq_transmute(mutate_fun = to.weekly)
-        
+
        },
        "M"={
-         print(paste0("Convirtiendo ",ticker," a frecuencia",periodicidad2,"..."))          
+         print(paste0("Convirtiendo ",ticker," a frecuencia",periodicidad,"..."))
          tablaDatos=tablaDatos%>%tq_transmute(mutate_fun = to.monthly)
        },
        "Q"={
-         print(paste0("Convirtiendo ",ticker," a frecuencia",periodicidad2,"..."))          
+         print(paste0("Convirtiendo ",ticker," a frecuencia",periodicidad,"..."))
          tablaDatos=tablaDatos%>%tq_transmute(mutate_fun = to.quarterly)
        },
        "Y"={
-         print(paste0("Convirtiendo ",ticker," a frecuencia",periodicidad2,"..."))          
+         print(paste0("Convirtiendo ",ticker," a frecuencia",periodicidad,"..."))
          tablaDatos=tablaDatos%>%tq_transmute(mutate_fun = to.yearly)
-       }        
+       }
 )
 
 
 # Agrega P/L, rendimientos y rendimientos contínuos:
 
-tablaDatos=tablaDatos%>%mutate(PL=adjusted / lag(adjusted), 
+tablaDatos=tablaDatos%>%mutate(PL=adjusted / lag(adjusted),
                                rArit=(adjusted/lag(adjusted)-1)*100,
                                rCont=(log(adjusted) - lag(log(adjusted)))*100)
 
 if (!(fxRate=="none")){
 stringSalida=paste0("objetoSalida=list(",nombre,"=tablaDatos,tablaFXYahoo=tablaDatosFX)")
-  
+
 } else {
   stringSalida=paste0("objetoSalida=list(",nombre,"=tablaDatos)")
-  
+
 }
 
 print(paste0(nombre," extraído de Yahoo Finance..."))
@@ -357,7 +357,7 @@ dateToUNIX <- function(Date) {
   trunc(as.numeric(posixct))
 }
 
-#convert UNIX timestamp to date 
+#convert UNIX timestamp to date
 # as.Date(as.POSIXct(x, origin="1970-01-01"))
 
 
